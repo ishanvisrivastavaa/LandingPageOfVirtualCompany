64b5518c9fc735fd65ca0f555afc61a347aca4b0
ishanvisrivastavaa <ishanvisrivastavaa7@gmail.com>
2024-10-16 11:47:42 +0530

    first

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../assets/logo.png";
+import { navItems } from "../constants";
 function Navbar() {
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blue-lg border-b border-neutral-700/80">
@@ -7,8 +8,11 @@ function Navbar() {
         <div className="flex justify-center items-center">
           <div className="flex items center flex-shrink-0">
             <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
-            <span className="text-xl tracking-tight"></span>
+            <span className="text-xl tracking-tight">VirtualHR</span>
           </div>
+                  <ul className="hidden lg:flex ml-14 space-x-12">
+                      
+          </ul>
         </div>
       </div>
     </nav>
