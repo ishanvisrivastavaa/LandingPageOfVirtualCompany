2d90255b6ad7b3ed11cb1fbd645a2c644646d098
ishanvisrivastavaa <ishanvisrivastavaa7@gmail.com>
2024-10-16 15:57:48 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Herpsection from "./components/Herpsection";
+import Featuresection from "./components/Featuresection";
+import Workflow from "./components/Workflow";
+import Pricingsection from "./components/Pricingsection";
+import Testimonial from "./components/Testimonial";
+import Footer from "./components/Footer";
+
 
 function App() {
-
   return (
     <>
-     <Navbar /> 
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <Herpsection />
+        <Featuresection />
+        <Workflow />
+        <Pricingsection />
+        <Testimonial />
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
